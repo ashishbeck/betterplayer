4d68ebd9165dc33a1f5918762bb041e4ea4e8257
ashishbeck <ashishbeck96@gmail.com>
2021-02-07 16:40:54 +0530

    setting keep alive to false
    This allows for the pageview in the upper widget tree to be successfully disposed

--- lib/src/list/better_player_list_video_player.dart
+++ lib/src/list/better_player_list_video_player.dart
@@ -117,5 +117,5 @@ class _BetterPlayerListVideoPlayerState
   String _getUniqueKey() => widget.dataSource.hashCode.toString();
 
   @override
-  bool get wantKeepAlive => true;
+  bool get wantKeepAlive => false;
 }
